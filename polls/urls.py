73b5c7a7e35311151fbb0ac415234be22b39c83b
Yucehan Kucukmotor <ychnkckmtr@gmail.com>
2018-10-17 22:54:08 -0400

    polling app from original python documentation/tutorial to be pushed to github. will proceed later for further exercise

---
+++ polls/urls.py
@@ -0,0 +1,32 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from . import views
+
+app_name = 'polls'
+
+urlpatterns = [
+    # ex: /polls/
+    path('', views.IndexView.as_view(), name='index'),
+    # ex: /polls/5
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # ex: /polls/5/results/
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+
+
+]
